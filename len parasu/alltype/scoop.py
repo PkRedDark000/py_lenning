fc975b58bf4da1beaae181ad43fb080527fd7554
Ramram43 <parasuraman4439@gmail.com>
2025-02-17 19:34:29 +0300

    update

---
+++ len parasu/alltype/scoop.py
@@ -0,0 +1,36 @@
+# Name = "parasuraman"
+# age = 18 
+# def another ():
+#     global age
+#     def greeting(name):
+#         print(name)
+#         print(age)
+#     greeting("Share")
+# another()
+
+# # color scoop
+# Name = "parasuraman"
+# age = 18 
+# def another ():
+#     global age
+#     color = "green"
+#     def greeting(name):
+#         print(name)
+#         print(age)
+#         print(color)
+#     greeting("Share")
+# another()
+
+# # local scoop color
+Name = "parasuraman"
+age = 18 
+def another ():
+    global age
+    color = "green"
+    def greeting(name):
+        color = "red"
+        print(name)
+        print(age)
+        print(color)
+    greeting("Share")
+another()
