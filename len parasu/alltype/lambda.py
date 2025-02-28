61d52d0c6b4e6c1d6ade9c982da935a6d12308d0
Ramram43 <parasuraman4439@gmail.com>
2025-02-28 20:24:43 +0300

    update

--- len parasu/alltype/lambda.py
+++ len parasu/alltype/lambda.py
@@ -21,3 +21,25 @@ add_twenty = funcBuilder(20)
 
 print(add_ten(7))
 print(add_twenty(7))
+
+
+numbers = [3, 7, 12, 18, 20, 21]
+
+squared_nums = map (lambda num : num * num, numbers)
+
+print(list(squared_nums))
+
+odd_nums = filter(lambda num: num % 2 != 0,numbers)
+print(list(odd_nums))
+
+from functools import reduce
+
+numbers = [1,2,3,4,5,1]
+totel = reduce(lambda acc, curr: acc + curr, numbers)
+print(totel)
+
+print(sum(numbers))
+
+names = ['parasu ','kaviya ','parthiban ']
+char_count = reduce(lambda acc, curr: acc + len (curr),names,0)
+print (char_count)
