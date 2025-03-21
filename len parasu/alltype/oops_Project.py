36010b1102c25bda42405818769e6da14b323ba6
Ramram43 <parasuraman4439@gmail.com>
2025-03-21 18:55:51 +0300

    update

--- len parasu/alltype/oops_Project.py
+++ len parasu/alltype/oops_Project.py
@@ -1,8 +1,17 @@
 # # # Bank Accounts
+# # step 1
+# class BankAccount:
+#     def __init__(self,initial_amount,account_name,account_number,):
+#         self.balance = initial_amount
+#         self.name = account_name
+#         self.account_number = account_number
+#         print(f"\n Account '{self.name}'created. \n Account Number = {self.account_number} \n Balance = ${self.balance} ")
+        
+# # step 2
 class BankAccount:
     def __init__(self,initial_amount,account_name,account_number,):
         self.balance = initial_amount
         self.name = account_name
         self.account_number = account_number
-        print(f"\n Account '{self.name}'created. \n Account Number = {self.account_number} \n Balance = ${self.balance} ")
+        print(f"\n Account '{self.name}'created. \n Account Number = {self.account_number} \n Balance = ${self.balance:.2f} ")
         
