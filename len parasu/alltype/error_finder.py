4af187acae79321eee90579188733ddf1ada6f6d
Ramram43 <parasuraman4439@gmail.com>
2025-03-21 06:20:18 +0300

    update

--- len parasu/alltype/error_finder.py
+++ len parasu/alltype/error_finder.py
@@ -34,3 +34,18 @@ else:
 finally:
     print("I'm going to print with or without an error.")
 
+## step 5
+x = 2
+try:
+    print(x /0)
+except NameError:
+    print("Name chihck with name")
+except ZeroDivisionError:
+    print("Please do not divide by zero.")
+except Exception as error:
+    print("error")
+
+else:
+    print("No Error")
+finally:
+    print("I'm going to print with or without an error.")
