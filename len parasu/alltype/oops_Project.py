45f98b7b2b97d3be75fcaead68278ea31780a4b7
Ramram43 <parasuraman4439@gmail.com>
2025-03-28 20:56:32 +0300

    update

--- len parasu/alltype/oops_Project.py
+++ len parasu/alltype/oops_Project.py
@@ -16,6 +16,24 @@
 #         print(f"\n Account '{self.name}'created. \n Account Number = {self.account_number} \n Balance = ${self.balance:.2f} ")
    
 #  # step 3 
+# class BankAccount:
+#     def __init__(self,initial_amount,account_name,account_number,):
+#         self.balance = initial_amount
+#         self.name = account_name
+#         self.account_number = account_number
+#         print(f"\n Account '{self.name}'created. \n Account Number = {self.account_number} \n Balance = ${self.balance:.2f} ")
+        
+#     def get_balance(self):
+#         print(f"\n Account'{self.name}' \n Acount number = {self.account_number}\n balance = ${self.balance:.2f}")
+#     # # update amount comment
+#     def deposit(self, amount):
+#         self.balance = self.balance + amount
+#         print("\n Deposit Cpomlete.")
+#         self.get_balance()
+
+#  # step 4
+class BalanceException(Exception):
+    pass
 class BankAccount:
     def __init__(self,initial_amount,account_name,account_number,):
         self.balance = initial_amount
@@ -25,8 +43,24 @@ class BankAccount:
         
     def get_balance(self):
         print(f"\n Account'{self.name}' \n Acount number = {self.account_number}\n balance = ${self.balance:.2f}")
-    #update amount comment
+    # # update amount comment
     def deposit(self, amount):
         self.balance = self.balance + amount
-        print("\n Deposit Cpomlete.")
+        print("\n Deposit Complete.")
         self.get_balance()
+    # # withdraw update comment
+    def viable_transaction(self, amount):
+        if self.balance >= amount:
+            return
+        else:
+            raise BalanceException(
+                f"\n Sorry , Account'{self.name}' only has a balance of ${self.balance:.2f} "
+            )
+    def Withdraw(self, amount):
+        try:
+            self.viable_transaction(amount)
+            self.balance = self.balance - amount
+            print("\n Withdraw Complete.")
+            self.get_balance()
+        except BalanceException as error:
+            print(f"\n Withdraw interrupted:{error}")
