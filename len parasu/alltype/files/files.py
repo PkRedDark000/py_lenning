6e1e45aee11055ab96472038e916d5cc92335f8f
Ramram43 <parasuraman4439@gmail.com>
2025-04-01 19:40:53 +0300

    update

--- len parasu/alltype/files/files.py
+++ len parasu/alltype/files/files.py
@@ -37,3 +37,40 @@ files = open("names.txt")
 # files.write("\ncss")
 # files.close()
 
+# step 6
+# f = open("context.txt","w")
+# f.write("I deleted all of the context")
+# f.close()
+
+# step 7
+# Two ways to create a new file
+# Opens a file writing, creates the file if it does not exist
+# f = open("name_list.txt","w")
+# f.close()
+
+# step 8
+if not os.path.exists("dave.txt"):
+    f = open("dave.txt","x")
+    f.close()
+
+# step 9
+# alrededy files is avalabi not a creats
+# f = open("dave.txt","x")
+# f.close()
+
+# step 10
+# Delete a files
+# Avoid an error if it doesn't exist
+if os.path.exists("dave.txt"):
+    os.remove("dave.txt")
+else:
+    print("The file you wish to delete does not exist")
+
+# step 11
+# With has built-in implicit exception handling
+# close() will be automatically called
+with open("more_named.txt") as f:
+    content = f.read()
+
+with open("names.txt", "w") as f:
+    f.write(content)
