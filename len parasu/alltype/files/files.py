dbda47aaad1d4e39577908351231cca2b4350ad8
Ramram43 <parasuraman4439@gmail.com>
2025-04-01 18:51:05 +0300

    update

---
+++ len parasu/alltype/files/files.py
@@ -0,0 +1,20 @@
+import os
+
+# r = Read
+# a = Append
+# w = Write
+# x = Create
+
+# Read - error if it doesn't exist 
+
+# step 1
+files = open("names.txt")
+# print(files.read())
+# print(files.read(4))
+
+# step 2
+# print(files.readline())
+# print(files.readline())
+# print(files.readline())
+
+# step 3
